e87e0ed05ab7d17600a7e446de9faf352b4e021d
colinju <colin.julien1@gmail.com>
2017-01-30 16:44:15 +0100

    building a good contract

--- generators/entity/templates/src/test/resources/contracts/_EntityContract.groovy
+++ generators/entity/templates/src/test/resources/contracts/_EntityContract.groovy
@@ -3,8 +3,37 @@ org.springframework.cloud.contract.spec.Contract.make {
 		method 'GET'
 		url '/api/<%=name%>s'
 		body([
-			body: "foo"
+			<%_ for (idx in fields) {
+				var fieldType = fields[idx].fieldType;
+				var fieldTypeBlobContent = fields[idx].fieldTypeBlobContent;
+				var isEnum = fields[idx].fieldIsEnum;
+				if (fieldType == 'String' || fieldTypeBlobContent == 'text') {_%>
+					
+				<%_ } else if (fieldType == 'Integer') { _%>
+				
+				<%_ } else if (fieldType == 'Long') { _%>
+				
+				<%_ } else if (fieldType == 'Float') { _%>
+
+				<%_ } else if (fieldType == 'Double') { _%>
+
+				<%_ } else if (fieldType == 'BigDecimal') { _%>
+
+				<%_ } else if (fieldType == 'UUID') { _%>
+
+				<%_ } else if (fieldType == 'LocalDate') { _%>
+
+				<%_ } else if (fieldType == 'ZonedDateTime') { _%>
+
+				<%_ } else if (fieldType == 'Boolean') { _%>
+
+				<%_ } else if ((fieldType == 'byte[]' || fieldType === 'ByteBuffer') && fieldTypeBlobContent != 'text') { _%>
+
+				<%_ } } _%>
 		])
+		headers {
+			contentType('application/json; charset=utf8')
+		}
 	}
 	response {
 		status 200
